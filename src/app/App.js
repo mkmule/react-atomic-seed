b3cc16d876cb797a47ea16f65c8fd1f2d281750a
Serhiy Moskovchuk <serhiy.moskovchuk@accenture.com>
2018-09-08 12:10:30 +0200

    Added basic routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
+import { Routes } from './App.constants';
 import { Home } from './components';
 
 class App extends React.Component {
   render() {
-    return <Home />;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={Routes.root}>
+            <Home />
+          </Route>
+
+          <Route>
+            <Redirect to={Routes.root} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
