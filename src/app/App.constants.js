4a04d09f2873e900216e91f543ceebe148f3b6c1
Serhiy Moskovchuk <serhiy.moskovchuk@accenture.com>
2018-09-13 21:27:45 +0200

    Added const for environment aware configurations

--- src/app/App.constants.js
+++ src/app/App.constants.js
@@ -1,3 +1,6 @@
+// You can use this const to add specific configurations for production environment
+// const productionEnvironment = (process.env.NODE_ENV === 'production');
+
 export const Routes = {
   root: '/',
 };
